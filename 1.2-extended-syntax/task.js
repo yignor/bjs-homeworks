67526771e16d682d2178b98e7d4d3c3073b743d6
yignor <58684900+yignor@users.noreply.github.com>
2020-04-01 18:29:35 +0300

    1st task

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,23 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    // код для задачи №1 писать здесь ax^2+bx+c=0
+    let d, x = [];
+     
+     if (a == 0) {
+         console.log('Не верное условие');
+     };
+
+    d = (Math.pow(b) - 4*a*c)
+
+    if (d < 0) {
+        x = 'Корней нет';
+    } else if (d == 0) {
+        x = (-b / 2*a);
+    } else if (d > 0) {
+        x[0] = ((-b+Math.sqrt(d)) / (2*a));
+        x[1] = ((-b-Math.sqrt(d)) / (2*a));
+        };
+    return x;
 }
 
 function getAverageMark(marks){
