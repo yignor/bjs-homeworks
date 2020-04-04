51f3224d9712b27c9efef21bb696322c97b1de24
yignor <58684900+yignor@users.noreply.github.com>
2020-04-04 20:06:17 +0300

    1st task test

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,24 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let totalAmount, creditBoddy, totalMonth, payment;
+	percent = parseInt(percent);
+	contribution = parseInt(contribution);
+	amount = parseInt(amount);
+	if (isNaN(percent)) {
+		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
+	} else if (isNaN(contribution)) {
+		return `Параметр "Начальный взонс" содержит неправильное значение ${contribution}`
+	} else if (isNaN(amount)) {
+		return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
+	} else if (new Date < date) {
+		return `Параметр даты введен не верно`
+	};
+	creditBoddy = amount - contribution;
+	totalMonth = ((date.getFullYear() - new Date.getFullYear()) * 12) + 1 + (date.getMonth() - new Date.getMonth()); //получаем разницу в месяцах между сегодня и датой выплаты
+	payment = creditBoddy * (percent + percent / (Math.pow(1+percent, totalMonth)-1));
+
+
+	return totalAmount = payment * totalMonth;
 }
 
 function getGreeting(name) {
