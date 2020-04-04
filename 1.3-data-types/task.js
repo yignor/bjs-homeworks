be53aa9e620cfb09f89a8f166b1105472683acb8
yignor <58684900+yignor@users.noreply.github.com>
2020-04-04 20:50:25 +0300

    full 2nd task and almost 1st task

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,27 +1,29 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
     let totalAmount, creditBoddy, totalMonth, payment;
-	percent = parseInt(percent) / 12 / 100;
-	contribution = parseInt(contribution);
-	amount = parseInt(amount);
-	if (isNaN(percent)) {
-		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`
-	} else if (isNaN(contribution)) {
-		return `Параметр "Начальный взонс" содержит неправильное значение ${contribution}`
-	} else if (isNaN(amount)) {
-		return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`
-	} else if (new Date < date) {
-		return `Параметр даты введен не верно`
+	if (isNaN(parseInt(percent))) {
+		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+	} else if (isNaN(parseInt(contribution))) {
+		return `Параметр "Начальный взонс" содержит неправильное значение ${contribution}`;
+	} else if (isNaN(parseInt(amount))) {
+		return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+	} else if (new Date > date) {
+		return `Параметр дата введен не верно`;
 	};
+	percent = percent / 12 / 100;
 	creditBoddy = amount - contribution;
-	totalMonth = ((date.getFullYear() - new Date.getFullYear()) * 12) + 1 + (date.getMonth() - new Date.getMonth()); //получаем разницу в месяцах между сегодня и датой выплаты
+	totalMonth = ((date.getFullYear() - new Date().getFullYear()) * 12) + 1 + (date.getMonth() - new Date().getMonth()); //получаем разницу в месяцах между сегодня и датой выплаты
 	payment = creditBoddy * (percent + percent / (Math.pow(1+percent, totalMonth)-1));
 
 
-	return totalAmount = payment * totalMonth;
+	return totalAmount = (payment * totalMonth).toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	let	greeting;
+    if ((name == "") || (name == null) || (!name == undefined)) {
+	   	return `Привет, мир! Меня зовут Аноним`;
+    };
+
+    return greeting = `Привет, мир! Меня зовут ${name}`;
 }
