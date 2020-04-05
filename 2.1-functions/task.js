6780599e3743e232164c75a5510b0ef1b79ecf1d
yignor <58684900+yignor@users.noreply.github.com>
2020-04-05 14:44:51 +0300

    1st task at all

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,15 +1,25 @@
 function getSolutions( a, b, c ) {
-	let d = (Math.pow(b, 2) - 4 * a * c);
-	let	roots = [];
-	if (d < 0) {
-		return {d: d, roots: []};
-	} else if (d = 0) {
+	let roots, D = (Math.pow(b, 2) - 4 * a * c);
+	if (D < 0) {
+		return {D: D, roots: []};
+	} else if (D == 0) {
 		let x1 = (- b / (2 * a));
-		return {d: d, roots: [x1]};
-	} else if (d > 0) {
-		let x1 = ((- b + Math.sqrt(d)) / (2 * a));
-		let	x2 = ((- b - Math.sqrt(d)) / (2 * a));
-		return {d: d, roots: [x1, x2]};
-
+		return {D: D, roots: [x1]};
+	} else if (D > 0) {
+		let x1 = ((- b + Math.sqrt(D)) / (2 * a));
+		let	x2 = ((- b - Math.sqrt(D)) / (2 * a));
+		return {D: D, roots: [x1, x2]};
 	};
-}; 
+};
+function showSolutionsMessage( a, b, c ) {
+	let result = getSolutions( a, b , c);
+	console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}\n`);
+	console.log(`Значение дискриминанта: ${result.D}`);
+	if (result.roots == []) {
+		console.log(`Уравнение не имеет вещественных корней`);
+	} else if (result.roots.length == 1) {
+		console.log(`Уравнение имеет один корень X₁ = ${result.roots}`)
+	} else if (result.roots.length == 2) {
+		console.log(`Уравнение имеет один корень X₁ =${result.roots[0]}, X₂ = ${result.roots[1]}`)
+	};
+};
