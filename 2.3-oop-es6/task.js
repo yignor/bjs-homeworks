08ef901ebfdbcefdcc5e0728b4ac9f00bd09b34e
yignor <58684900+yignor@users.noreply.github.com>
2020-04-08 13:51:48 +0300

    update 1st task

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,21 +1,21 @@
-'use strict';
+
 class Weapon {
-	constructor(name, attack, durability, range,) {
-		this.name = name;
-		this.attack = attack;
-		this.durability = durability;
-		this.range = range;
+	constructor(Weapon) {
+		this.name = Weapon.name;
+		this.attack = Weapon.attack;
+		this.durability = Weapon.durability;
+		this.range = Weapon.range;
 	};
 	takeDamage(damage) {
 		this.durability -= damage;
 		if (this.durability < 0) {
-			return 0;
+			return this.durability = 0;
 		}
 		return this.durability;
 	};
 	getDamage() {
 		if (this.durability == 0) {
-			return 0;
+			return this.durability = 0;
 		} else if (this.durability >= this.durability * 0.3) {
 			return this.attack;
 		} else {
@@ -25,14 +25,16 @@ class Weapon {
 	isBroken() {
 		if (this.durability > 0) {
 			return false;
-		} 
-	}
+		} else {
+			return true;
+		};
+	};
 };
 
 const sword = new Weapon({
   name: 'Старый меч',
   attack: 20,
-  durability: 110,
+  durability: 10,
   range: 1,
 });
 
@@ -41,3 +43,38 @@ console.log(sword.durability); // 5
 
 sword.takeDamage(50);
 console.log(sword.durability); // 0
+
+const arm = new Weapon({
+  name: 'Рука',
+  attack: 1,
+  durability: Infinity,
+  range: 1,
+});
+
+arm.takeDamage(20);
+console.log(arm.durability); // Infinity
+
+const bow = new Weapon({
+  name: 'Лук',
+  attack: 10,
+  durability: 200,
+  range: 3,
+});
+
+bow.takeDamage(20);
+console.log(bow.durability); // 180
+
+bow.takeDamage(200);
+console.log(bow.durability); // 0
+
+console.log(bow.durability); // 0
+console.log(bow.getDamage()); // 0
+
+console.log(arm.durability); // Infinity
+console.log(bow.getDamage()); // 1
+
+console.log(bow.durability); // 0
+console.log(bow.isBroken()); // true
+
+console.log(arm.durability); // Infinity
+console.log(bow.isBroken()); // false
