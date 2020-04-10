63f7a1487523b2e47f3d3f1e0ba5a3418babeeec
yignor <58684900+yignor@users.noreply.github.com>
2020-04-10 20:53:41 +0300

    2nd task

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,9 +1,9 @@
 //task 1
 function parseCount (count) {
-	if (isNaN(Number.parseInt(count))) {
+	if (isNaN(count)) {
 		throw new Error("Невалидное значение");
-	} 
-	return count;
+	}; 
+	return Number.parseInt(count);
 };
 function validateCount (count) {
 	try {
@@ -14,3 +14,33 @@ function validateCount (count) {
 return result;
 };
 //task 2
+class Triangle {
+	constructor (a, b, c,) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if ((this.a + this.b < this.c) || (this.a + this.c < this.b) || (this.b + this.c) < this.a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		};
+	};
+	getPerimeter() {
+		let perimeter = this.a + this.b + this.c;
+		return perimeter;
+	};
+	getArea() {
+		let p = this.getPerimeter() / 2
+		let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return parseFloat(s.toFixed(3));
+	};
+
+};
+function getTriangle(a, b, c,) {
+	try {
+		const triangle = new Triangle(a, b, c);
+		return triangle;
+	} catch (e) {
+		let object = {};
+		object.getPerimeter = object.getArea = () => "Ошибка! Неправильный треугольник";
+		return object;
+	}
+};
