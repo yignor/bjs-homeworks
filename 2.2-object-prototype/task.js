5ae0cf4edb799aaab7a27b119822e8d9a2372d92
Yury Sh <yignor@gmail.com>
2020-11-19 12:13:11 +0300

    edit solution

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,13 +1,26 @@
 //String.prototype.isPalindrome - для задачи №1
 
 String.prototype.isPalindrome = function() {
-	let str = this.toLowerCase();
-	for (i = 0; i < str.length; i++) {
-		return (str[i] === str[str.length - 1 - i]) ? true : false;
-	};
+	// let str = this.toLowerCase().replace(/[\W_]/g, '');
+	// let reverseStr = str.split('').reverse().join(''); 
+	// return (reverseStr === str) ? true : false;
+	// let str = this.toLowerCase().replace(/[\W_]/g, '');
+	// for (let i = 0; i < str.length/2; i++) {
+	// 	if (str[i] !== str[str.length - 1 - i]) {
+	// 		console.log(str)
+	// 		return false;
+	// 	};
+	// };
+	// return (str[i] === str[str.length - 1 - i]) ? true : false;
+	let str = this.toLowerCase().replace(/\s+/g,'');
+	let revStr = str.split("").reverse().join("");
+	return (str === revStr) ? true : false;
 };
+console.log("А роза упала на лапу Азора")
 
-// Task 2
+	
+
+	// Task 2
 function getAverageMark(marks) {
 	let marksSum = 0
 	for (i = 0; i < marks.length; i++) {
