11291b0038af33a7495a01766512f73ee70ed534
yignor <58684900+yignor@users.noreply.github.com>
2020-11-28 23:49:49 +0300

    continue

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -3,24 +3,22 @@ console.clear();
 
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-// const task1 = weapons.find(getNames => getNames.name != null);
-// console.log(task1)
-
 function getNames(arr) {
-    if (getNames.name != null) {
-        return getNames.name;
-    } else {
-        return 'Errore';
-    };
+    return weapons.map(arr => arr.name);
 };
 
-console.log(getNames(weapons))
-// console.log(1)
-// function getNames(arr) {
-//     return arr.every(arr.name != null);
-// };
-// console.log(getNames(weapons))
+function getCountReliableWeapons(value) {
+    return weapons.filter(weapons.length(weapons.filter => weapons.durability > value))
+};
 
-// function getCountReliableWeapons(arr) {
-    //     return arr.
-    // }
+function hasReliableWeapons() {
+
+};
+
+function getReliableWeaponsNames() {
+    
+};
+
+function getTotalDamage() {
+
+};
--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,80 +1,80 @@
-//task 1
-function sleep(milliseconds) 
-{
-  let e = new Date().getTime() + milliseconds;
-  while (new Date().getTime() <= e) {}
-};
-function sum(...args) {
-  // Замедление на половину секунды.
-  sleep(500); // Можно использовать другое значение замедления.
-  return args.reduce((sum, arg) => {
-    return sum += +arg;
-  }, 0);
-};
-
-
-function compareArrays( arr1, arr2 ) {
- if ((arr1.length == arr2.length) && (arr1.every())) {
- };
-};
-
-// function compareArrays (arr1, arr2) {
-// 	if ((arr1.length == arr2.length) && (arr1.every((element, index, array ) => element === arr2[index])))  {
-// 		console.log(true)
-//     return true;
-// 	}
-//   console.log(false)
-// 	return false;
+// //task 1
+// function sleep(milliseconds) 
+// {
+//   let e = new Date().getTime() + milliseconds;
+//   while (new Date().getTime() <= e) {}
+// };
+// function sum(...args) {
+//   // Замедление на половину секунды.
+//   sleep(500); // Можно использовать другое значение замедления.
+//   return args.reduce((sum, arg) => {
+//     return sum += +arg;
+//   }, 0);
 // };
 
-// // compareArrays([8, 9], [6]); // false, разные значения
-// // compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
-// // compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
-// // compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
-// // compareArrays([8, 1, 2], [8, 1, 2]); // true
-// function memorize(fn, limit) {
-//   const results = [];
 
-//   return (...args) => {
-//     let checkMemory = results.find(item => compareArrays(item.args, Array.from(args)));
+// function compareArrays( arr1, arr2 ) {
+//  if ((arr1.length == arr2.length) && (arr1.every())) {
+//  };
+// };
 
-//     if (checkMemory) {
-//       return results.result;
-//     } else {
-//       let result = fn(...args);
-//       results.push({args: Array.from(args), result: result});
+// // function compareArrays (arr1, arr2) {
+// // 	if ((arr1.length == arr2.length) && (arr1.every((element, index, array ) => element === arr2[index])))  {
+// // 		console.log(true)
+// //     return true;
+// // 	}
+// //   console.log(false)
+// // 	return false;
+// // };
 
-//       if (results.length > limit) {
-//         results.shift();
-//       }
+// // // compareArrays([8, 9], [6]); // false, разные значения
+// // // compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
+// // // compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
+// // // compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
+// // // compareArrays([8, 1, 2], [8, 1, 2]); // true
+// // function memorize(fn, limit) {
+// //   const results = [];
 
-//       return result;
-//     }
-//   }
-// }
+// //   return (...args) => {
+// //     let checkMemory = results.find(item => compareArrays(item.args, Array.from(args)));
+
+// //     if (checkMemory) {
+// //       return results.result;
+// //     } else {
+// //       let result = fn(...args);
+// //       results.push({args: Array.from(args), result: result});
+
+// //       if (results.length > limit) {
+// //         results.shift();
+// //       }
+
+// //       return result;
+// //     }
+// //   }
+// // }
 
 
-// function testCase (testFunction, timer) {
-//   const testArray = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
-//   console.time(timer);
-//   for (let i = 0; i < 10; i++){
-//     testArray.forEach((testArray) => testFunction(...testArray));
-//   }
-//   console.timeEnd(timer);
-// }
+// // function testCase (testFunction, timer) {
+// //   const testArray = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
+// //   console.time(timer);
+// //   for (let i = 0; i < 10; i++){
+// //     testArray.forEach((testArray) => testFunction(...testArray));
+// //   }
+// //   console.timeEnd(timer);
+// // }
 
 
 
-// const mSum = memorize(sum, 5); // 5 результатов может хранится в памяти
+// // const mSum = memorize(sum, 5); // 5 результатов может хранится в памяти
 
-// // Вызов этих функций даёт один и тот же результат
-// sum(3, 4); // 7
-// /* 
-//   разница только в том, что mSum запоминает результат (7)
-//   и повторно не делает вычисления
-//  */
-// mSum(3, 4); // 7
-// console.log(mSum)
-// testCase(sum,'Проверка функции sum');
+// // // Вызов этих функций даёт один и тот же результат
+// // sum(3, 4); // 7
+// // /* 
+// //   разница только в том, что mSum запоминает результат (7)
+// //   и повторно не делает вычисления
+// //  */
+// // mSum(3, 4); // 7
+// // console.log(mSum)
+// // testCase(sum,'Проверка функции sum');
 
-// testCase(mSum,'Проверка оптимизированной версии');
+// // testCase(mSum,'Проверка оптимизированной версии');
