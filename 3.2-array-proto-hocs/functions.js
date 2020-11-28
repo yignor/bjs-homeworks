59a16289f0e2a57d080f336c30b751b32e103743
 Y <y@MacBook-Pro-Y.local>
2020-11-28 14:12:31 +0300

    1st steps

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,26 @@
+// task 1
+console.clear();
+
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+
+// const task1 = weapons.find(getNames => getNames.name != null);
+// console.log(task1)
+
+function getNames(arr) {
+    if (getNames.name != null) {
+        return getNames.name;
+    } else {
+        return 'Errore';
+    };
+};
+
+console.log(getNames(weapons))
+// console.log(1)
+// function getNames(arr) {
+//     return arr.every(arr.name != null);
+// };
+// console.log(getNames(weapons))
+
+// function getCountReliableWeapons(arr) {
+    //     return arr.
+    // }
