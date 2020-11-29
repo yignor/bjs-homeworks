aef6f38823dde5974ee6987406abceb5f43e886a
yignor <58684900+yignor@users.noreply.github.com>
2020-11-30 01:48:41 +0300

    some add's

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -26,6 +26,27 @@ function getReliableWeaponsNames(value) {
 };
 
 function getTotalDamage() {
-    return weapons.reduce((acc, arr) => {return acc + arr.attack}, 0)
+    return weapons.reduce((acc, arr) => {return acc + arr.attack}, 0);
 };
-console.log(getTotalDamage())
+
+//task 2
+
+function sleep(milliseconds) 
+{
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+};
+
+function sum(...args) {
+    // Замедление на половину секунды.
+    sleep(100); // Можно использовать другое значение замедления.
+    return args.reduce((sum, arg) => {
+      return sum += +arg;
+    }, 0);
+  };
+
+compareArrays([8, 9], [6]); // false, разные значения
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]); // false, разные значения
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]); // false, разные значения
+compareArrays([1, 2, 3], [2, 3, 1]); // false, разные индексы, хотя и одинаковые значения
+compareArrays([8, 1, 2], [8, 1, 2]); // true
